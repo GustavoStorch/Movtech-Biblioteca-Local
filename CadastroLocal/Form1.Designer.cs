4b2514710524cd42958f8ec641fc1bf18d50e451
Gustavo Kauan Storch <112282756+GustavoStorch@users.noreply.github.com>
2023-05-11 10:23:46 -0300

    Add files via upload

---
+++ CadastroLocal/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace CadastroLocal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblCadLocal = new System.Windows.Forms.Label();
+            this.lblCodLocal = new System.Windows.Forms.Label();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.txtCodLocal = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.gridCadLocal = new System.Windows.Forms.DataGridView();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCadLocal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCadLocal
+            // 
+            this.lblCadLocal.AutoSize = true;
+            this.lblCadLocal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadLocal.Location = new System.Drawing.Point(501, 41);
+            this.lblCadLocal.Name = "lblCadLocal";
+            this.lblCadLocal.Size = new System.Drawing.Size(215, 36);
+            this.lblCadLocal.TabIndex = 0;
+            this.lblCadLocal.Text = "Cadastro Local";
+            // 
+            // lblCodLocal
+            // 
+            this.lblCodLocal.AutoSize = true;
+            this.lblCodLocal.Location = new System.Drawing.Point(128, 145);
+            this.lblCodLocal.Name = "lblCodLocal";
+            this.lblCodLocal.Size = new System.Drawing.Size(63, 20);
+            this.lblCodLocal.TabIndex = 1;
+            this.lblCodLocal.Text = "Código:";
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Location = new System.Drawing.Point(128, 223);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(84, 20);
+            this.lblDescricao.TabIndex = 2;
+            this.lblDescricao.Text = "Descrição:";
+            this.lblDescricao.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtCodLocal
+            // 
+            this.txtCodLocal.Enabled = false;
+            this.txtCodLocal.Location = new System.Drawing.Point(288, 142);
+            this.txtCodLocal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCodLocal.MaxLength = 3;
+            this.txtCodLocal.Name = "txtCodLocal";
+            this.txtCodLocal.Size = new System.Drawing.Size(205, 26);
+            this.txtCodLocal.TabIndex = 4;
+            this.txtCodLocal.TextChanged += new System.EventHandler(this.txtCodLocal_TextChanged);
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(288, 220);
+            this.txtDescricao.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDescricao.MaxLength = 50;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(796, 26);
+            this.txtDescricao.TabIndex = 5;
+            // 
+            // gridCadLocal
+            // 
+            this.gridCadLocal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridCadLocal.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridCadLocal.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.gridCadLocal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridCadLocal.Location = new System.Drawing.Point(132, 374);
+            this.gridCadLocal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gridCadLocal.Name = "gridCadLocal";
+            this.gridCadLocal.RowHeadersWidth = 62;
+            this.gridCadLocal.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridCadLocal.Size = new System.Drawing.Size(952, 309);
+            this.gridCadLocal.TabIndex = 10;
+            this.gridCadLocal.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gridCadLocal_MouseDoubleClick);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(679, 288);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(176, 57);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(349, 288);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(176, 57);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 710);
+            this.Controls.Add(this.gridCadLocal);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtCodLocal);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.lblCodLocal);
+            this.Controls.Add(this.lblCadLocal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Novo Local";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridCadLocal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCadLocal;
+        private System.Windows.Forms.Label lblCodLocal;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.TextBox txtCodLocal;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.DataGridView gridCadLocal;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ CadastroLocal/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroLocal
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlConnection conn;
+        private bool btnAtivo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCodLocal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table_load();
+            CarregaID();
+            btnAtivo = false;
+            botaoAtivado();
+        }
+
+        //Cria a conexão com o banco de dados.
+        private SqlConnection Conexao()
+        {
+            conn = new SqlConnection(@"Data Source=localhost\sqlexpress;Initial Catalog=Treinamento;Integrated Security=True");
+            return conn;
+        }
+
+        //Método para ativar ou desativar o botão de excluir do usuário.
+        private void botaoAtivado()
+        {
+            if (btnAtivo)
+            {
+                btnExcluir.Enabled = true;
+            }
+            else
+            {
+                btnExcluir.Enabled = false;
+            }
+        }
+
+        //Recupera o próximo id a ser cadastrado e joga ele para o textBox.
+        private void CarregaID()
+        {
+            conn = Conexao();
+            conn.Open();
+
+            SqlCommand cm = new SqlCommand("SELECT IDENT_CURRENT('mvtBibLocal') + 1", conn);
+            int nextCod = Convert.ToInt32(cm.ExecuteScalar());
+
+            txtCodLocal.Text = nextCod.ToString();
+            conn.Close();
+        }
+
+        //Botão com a funcionalidade de salvar/persistir os dados inseridos no banco de dados.
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            conn = Conexao();
+            String sql;
+
+            try
+            {
+                //Verifica se o campo do código está vazio e realiza o insert.
+                if (string.IsNullOrEmpty(this.txtCodLocal.Text))
+                {
+                    sql = "INSERT INTO mvtBibLocal(descricaoLocal) VALUES(@descricaoLocal)";
+                    SqlCommand c = new SqlCommand(sql, conn);
+
+                    if (String.IsNullOrWhiteSpace(txtDescricao.Text))
+                    {
+                        MessageBox.Show("Erro: Preencha a descrição do local!");
+                    }
+                    else
+                    {
+                        c.Parameters.Add(new SqlParameter("@descricaoLocal", this.txtDescricao.Text));
+                    }
+
+                    conn.Open();
+                    c.ExecuteNonQuery();
+                    conn.Close();
+
+                    MessageBox.Show("Enviado com sucesso!");
+
+                    limparForm();
+                    table_load();
+                    CarregaID();
+                }
+                else
+                {
+                    //Verifica se o código presente no textbox já está registrado dentro do banco de dados.
+                    conn.Open();
+                    string sql2 = "SELECT COUNT(*) FROM mvtBibLocal WHERE codLocal = @codLocal";
+                    SqlCommand cmdSelect = new SqlCommand(sql2, conn);
+                    cmdSelect.Parameters.AddWithValue("@codLocal", txtCodLocal.Text);
+                    int count = Convert.ToInt32(cmdSelect.ExecuteScalar());
+                    conn.Close();
+
+                    //Se o código estiver registrado no banco de dados realiza apenas o update.
+                    if (count > 0)
+                    {
+                        sql = "UPDATE mvtBibLocal SET descricaoLocal = @descricaoLocal WHERE codLocal = @codLocal";
+                        SqlCommand c = new SqlCommand(sql, conn);
+
+                        c.Parameters.AddWithValue("@codLocal", txtCodLocal.Text);
+
+                        if (String.IsNullOrWhiteSpace(txtDescricao.Text))
+                        {
+                            MessageBox.Show("Erro: Preencha a descrição do local!");
+                        }
+                        else
+                        {
+                            c.Parameters.Add(new SqlParameter("@descricaoLocal", this.txtDescricao.Text));
+                        }
+                        conn.Open();
+
+                        c.ExecuteNonQuery();
+
+                        conn.Close();
+
+                        MessageBox.Show("Atualizado com sucesso!");
+
+                        limparForm();
+                        table_load();
+                        CarregaID();
+                        btnAtivo = false;
+                        botaoAtivado();
+                    }
+                    else
+                    {
+                        //Se não estiver registrado no banco de dados realiza o insert.
+                        sql = "INSERT INTO mvtBibLocal(descricaoLocal) VALUES(@descricaoLocal)";
+                        SqlCommand c = new SqlCommand(sql, conn);
+
+                        if (String.IsNullOrWhiteSpace(txtDescricao.Text))
+                        {
+                            MessageBox.Show("Erro: Preencha a descrição do local!");
+                        }
+                        else
+                        {
+                            c.Parameters.Add(new SqlParameter("@descricaoLocal", this.txtDescricao.Text));
+                        }
+
+                        conn.Open();
+                        c.ExecuteNonQuery();
+                        conn.Close();
+
+                        MessageBox.Show("Enviado com sucesso!");
+
+                        limparForm();
+                        table_load();
+                        CarregaID();
+                    }
+                }
+            } catch(SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro: " + ex);
+            } finally
+            {
+                conn.Close();
+            }
+        }
+
+        //Botão que realiza o Delete de um registro no banco de dados.
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            conn = Conexao();
+            String sql = "DELETE mvtBibLocal WHERE codLocal = @codLocal";
+
+            try
+            {
+                SqlCommand c = new SqlCommand(sql, conn);
+
+                c.Parameters.AddWithValue("@codLocal", txtCodLocal.Text);
+
+                conn.Open();
+                c.ExecuteNonQuery();
+
+                limparForm();
+                table_load();
+                CarregaID();
+                //btnAtivo = false;
+                botaoAtivado();
+
+                MessageBox.Show("Excluído com sucesso!");
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        //Método que limpa o formulário toda vez que é salvo ou deletado um registro.
+        public void limparForm()
+        {
+            txtCodLocal.Text = String.Empty;
+            txtDescricao.Text = String.Empty;
+        }
+
+        //Carrega todos os registros contidos no banco de dados para a DataGridView.
+        private void table_load()
+        {
+            conn = Conexao();
+            String sql = "SELECT codLocal AS Código, descricaoLocal AS Descrição FROM mvtBibLocal ORDER BY Descrição";
+
+            try
+            {
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+
+                conn.Open();
+                da.Fill(ds);
+                gridCadLocal.DataSource = ds.Tables[0];
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        //Método que realiza o double click em uma linha da grid e joga todos os seus dados para as textBox.
+        private void gridCadLocal_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (gridCadLocal.SelectedRows.Count > 0)
+            {
+                txtCodLocal.Text = gridCadLocal.SelectedRows[0].Cells[0].Value.ToString();
+                txtDescricao.Text = gridCadLocal.SelectedRows[0].Cells[1].Value.ToString();
+
+                if (string.IsNullOrEmpty(this.txtDescricao.Text))
+                {
+                    btnExcluir.Enabled = false;
+                    CarregaID();
+                }
+                else
+                {
+                    btnExcluir.Enabled = true;
+                }
+
+            }
+        }
+    }
+}
